4f8fc2af01e9370127d41eac382ff25e2e562cb0
ADMIN <admin@ADMINs-Mac-mini.local>
2022-10-24 14:27:25 -0500

    end of class Monday

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const postBank = require('./postBank')
 const app = express()
 
 app.use(morgan('dev'))
+app.use(express.static('public'))
 
 app.get("/", (req, res) => {
   const posts = postBank.list()
@@ -12,14 +13,32 @@ app.get("/", (req, res) => {
   <html>
     <head>
       <title> Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
     </head>
     <body>
-      <ul>
-        ${posts.map(post =>  `<li>${post.title}, ${post.content},${post.name}, ${post.date} </li>`)}
-      </ul>
-    </body>
-  </html>`
+    <div class="news-list">
+    <header><img src="/logo.png"/>Wizard News</header>
+    ${posts.map(post => `
+    <div class='news-item'>
+      <p>
+        <span class="news-position">${post.id}. ▲</span>${post.title}
+        <small>(by ${post.name})</small>
+      </p>
+      <small class="news-info">
+        ${post.upvotes} upvotes | ${post.date}
+      </small>
+    </div>`
+  ).join('')}
+</div>
+</body>
+</html>`
   res.send(html)
+})
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  const html = 
+  res.send(``)
 })
 
 const PORT = 3000;
