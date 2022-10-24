35400226ef9df5e773af4d514e61b585f3610844
charlesvaldezz94 <charlesvaldezz94@gmail.com>
2022-10-24 13:56:23 -0500

    got to Static Routing

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const morgan = require('morgan')
+const postBank = require('./postBank')
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const app = express()
+
+app.use(morgan('dev'))
+
+app.get("/", (req, res) => {
+  const posts = postBank.list()
+  const html = `<!DOCTYPE html>
+  <html>
+    <head>
+      <title> Wizard News</title>
+    </head>
+    <body>
+      <ul>
+        ${posts.map(post =>  `<li>${post.title}, ${post.content},${post.name}, ${post.date} </li>`)}
+      </ul>
+    </body>
+  </html>`
+  res.send(html)
+})
 
 const PORT = 3000;
 
