53f06e7872bf1dfadfd3492f77204ae19472a584
ADMIN <admin@ADMINs-Mac-mini.local>
2022-10-25 11:10:05 -0500

    commit with errors from String refactor

--- app.js
+++ app.js
@@ -1,38 +1,17 @@
-const express = require("express")
-const morgan = require('morgan')
-const postBank = require('./postBank')
+const express = require("express");
+const morgan = require('morgan');
+const postBank = require('./postBank');
+const postList = require("./postList")
+
 
 const app = express()
 
 app.use(morgan('dev'))
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'))
 
 app.get("/", (req, res) => {
-  const posts = postBank.list()
-  const html = `<!DOCTYPE html>
-  <html>
-    <head>
-      <title> Wizard News</title>
-      <link rel="stylesheet" href="/style.css" />
-    </head>
-    <body>
-    <div class="news-list">
-    <header><img src="/logo.png"/>Wizard News</header>
-    ${posts.map(post => `
-    <div class='news-item'>
-      <p><a href="/posts/${post.id}">${post.title}</a>
-        <span class="news-position">${post.id}. ▲</span>
-        <small>(by ${post.name})</small>
-      </p>
-      <small class="news-info">
-        ${post.upvotes} upvotes | ${post.date}
-      </small>
-    </div>`
-  ).join('')}
-</div>
-</body>
-</html>`
-  res.send(html)
+  const posts = postBand.list()
+  res.send(postList(posts));
 })
 
 
---
+++ postList.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const morgan = require('morgan');
+
+const postList = express()
+
+postList.use(morgan('dev'))
+postList.use(express.static('public'))
+
+postList.get("/", (req, res) => {
+    const posts = postBank.list()
+    const html = `<!DOCTYPE html>
+    <html>
+      <head>
+        <title> Wizard News</title>
+        <link rel="stylesheet" href="/style.css" />
+      </head>
+      <body>
+      <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+      <div class='news-item'>
+        <p><a href="/posts/${post.id}">${post.title}</a>
+          <span class="news-position">${post.id}. ▲</span>
+          <small>(by ${post.name})</small>
+        </p>
+        <small class="news-info">
+          ${post.upvotes} upvotes | ${post.date}
+        </small>
+      </div>`
+    ).join('')}
+  </div>
+  </body>
+  </html>`
+    res.send(html);
+  })
+
+const PORT = 3000;
+
+postList.listen(PORT, () => {
+  console.log(`PostList listening in port ${PORT}`);
+});
